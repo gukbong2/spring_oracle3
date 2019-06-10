c2d554d03f1b2d758878967dbbb6aa232ba096fe
gukbong2 <junyeon2459@gmail.com>
2019-06-10 22:51:49 +0800

    20190610

---
+++ oraclePart3to7/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -0,0 +1,39 @@
+package org.zerock.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTests {
+	
+	@Setter(onMethod_ = {@Autowired})
+	private BoardMapper mapper;
+	
+	@Test
+	public void testGetList() {
+		mapper.getList().forEach(board -> log.info(board));
+	}
+	
+	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("title");
+		board.setContent("content");
+		board.setWriter("gukbong");
+		
+		mapper.insert(board);
+		
+		log.info(board);
+	}
+	
+	
+}
