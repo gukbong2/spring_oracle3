f7b43780998a5fba8e19d716273bf7b699788bc5
gukbong2 <junyeon2459@gmail.com>
2019-06-10 23:18:57 +0800

    20190611

--- oraclePart3to7/src/main/java/org/zerock/mapper/BoardMapper.java
+++ oraclePart3to7/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -13,9 +13,11 @@ public interface BoardMapper {
 	
 	public void insertSelectKey(BoardVO board);
 	
+	public BoardVO read(Long bno);
 	
+	public int delete(Long bno);
 	
-	
+	public int update(BoardVO board);
 	
 	
 	
--- oraclePart3to7/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ oraclePart3to7/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -18,22 +18,55 @@ public class BoardMapperTests {
 	@Setter(onMethod_ = {@Autowired})
 	private BoardMapper mapper;
 	
-	@Test
-	public void testGetList() {
-		mapper.getList().forEach(board -> log.info(board));
-	}
+//	@Test
+//	public void testGetList() {
+//		mapper.getList().forEach(board -> log.info(board));
+//	}
+//	
+//	@Test
+//	public void testInsert() {
+//		BoardVO board = new BoardVO();
+//		board.setTitle("title");
+//		board.setContent("content");
+//		board.setWriter("gukbong");
+//		
+//		mapper.insert(board);
+//		
+//		log.info(board);
+//	}
 	
+//	@Test
+//	public void testInsertSelectKey() {
+//		BoardVO board = new BoardVO();
+//		board.setTitle("새로 작성하는 글 select key");
+//		board.setContent("새로 작성하는 내용 select key");
+//		board.setWriter("gukbong");
+//		mapper.insertSelectKey(board);
+//		
+//		log.info(board);
+//	}
+
+//	@Test
+//	public void testRead() {
+//		BoardVO board = mapper.read(5L);
+//		
+//		log.info(board);
+//	}
+//	
+//	@Test
+//	public void testDelete() {
+//		log.info("DELETE COUNT : " + mapper.delete(5L));
+//	}
+
 	@Test
-	public void testInsert() {
+	public void testUpdate() {
 		BoardVO board = new BoardVO();
-		board.setTitle("title");
-		board.setContent("content");
-		board.setWriter("gukbong");
+		board.setBno(2L);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("user00");
 		
-		mapper.insert(board);
-		
-		log.info(board);
+		int count = mapper.update(board);
+		log.info("===UPDATE COUNT : " + count);
 	}
-	
-	
 }
