9b87e5200082714693fd5d46e944eb1f8dbd62c6
gukbong2 <junyeon2459@gmail.com>
2019-06-17 20:07:49 +0900

    20190617

--- oraclePart3to7/src/main/java/org/zerock/domain/ReplyVO.java
+++ oraclePart3to7/src/main/java/org/zerock/domain/ReplyVO.java
@@ -6,12 +6,13 @@ import lombok.Data;
 
 @Data
 public class ReplyVO {
-	
-	private Long rno, bno;
-	
-	private String reply, replyer;
-	
-	private Date replyDate, updateDate;
-	
-	
+
+  private Long rno;
+  private Long bno;
+
+  private String reply;
+  private String replyer;
+  private Date replyDate;
+  private Date updateDate;
+
 }
--- oraclePart3to7/src/test/java/org/zerock/mapper/ReplyMapperTests.java
+++ oraclePart3to7/src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -1,32 +1,53 @@
 package org.zerock.mapper;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.zerock.mapper.ReplyMapper;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.zerock.domain.Criteria;
+import org.zerock.domain.ReplyVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+// Java Config
+// @ContextConfiguration(classes = { org.zerock.config.PersistenceConfig.class
+// })
 @Log4j
 public class ReplyMapperTests {
-	
-	@Setter(onMethod_ = {@Autowired})
-	
+
+	// 테스트 전에 해당 번호의 게시물이 존재하는지 반드시 확인할 것
+	private Long[] bnoArr = { 2293869L, 2293868L, 2293867L, 2293866L, 2293865L };
+
+	@Setter(onMethod_ = @Autowired)
 	private ReplyMapper mapper;
-	
+
 	@Test
-	public void testMapper() {
-		log.info(mapper);
+	public void testCreate() {
+
+		IntStream.rangeClosed(1, 10).forEach(i -> {
+
+			ReplyVO vo = new ReplyVO();
+
+			// 게시물의 번호
+			vo.setBno(bnoArr[i % 5]);
+			vo.setReply("댓글 테스트 " + i);
+			vo.setReplyer("replyer" + i);
+
+			mapper.insert(vo);
+		});
+
 	}
 
-	
-	
-}	
-	
 
 	
+	
+
+
+}
