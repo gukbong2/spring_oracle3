79b35dde99017f6b22992fd7a051e83999438e37
gukbong2 <junyeon2459@gmail.com>
2019-07-06 23:01:44 +0900

    20190706

--- oraclePart3to7/src/main/java/org/zerock/task/FileCheckTask.java
+++ oraclePart3to7/src/main/java/org/zerock/task/FileCheckTask.java
@@ -7,7 +7,6 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
